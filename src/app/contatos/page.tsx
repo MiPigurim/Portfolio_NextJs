4b7dedaba56dc8b0350b3eb0f4e87ce1b53d26f9
MiPigurim <michelepigurim26@gmail.com>
2024-05-30 16:57:45 -0300

    feat: add page contatos

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contatos = () => {
+  return (
+    <div>
+      <h1>Contatos</h1>
+      <a href="/">Ir para a página inicial</a>
+      
+    </div>
+  )
+}
+
+export default Contatos;
